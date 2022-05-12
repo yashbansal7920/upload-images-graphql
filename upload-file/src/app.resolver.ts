c9f0c0d00537fce68db1cf3a57aeb41a7e072b90
Yash Bansal <76593329+yashbansal7920@users.noreply.github.com>
2022-05-12 15:48:10 +0530

    implement functions for different type of buckets

--- upload-file/src/app.resolver.ts
+++ upload-file/src/app.resolver.ts
@@ -1,26 +1,65 @@
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { FileUpload, GraphQLUpload } from 'graphql-upload';
-import { createWriteStream } from 'fs';
+import { Storage } from '@google-cloud/storage';
+import * as path from 'path';
 
+const storage = new Storage({
+  projectId: 'yash-dev-349706',
+  keyFilename: path.join(__dirname, '../src/yash-dev-349706-e210d8cb7b3b.json'),
+});
+
+// Access Control - Uniform (Private)
+const bucket = storage.bucket('yash-boldcare-testing');
+
+// Access Control - Uniform (Public)
+const bucket1 = storage.bucket('yash-boldcare-testing-public');
+
+// Access Control - Fine Grained
+const bucket2 = storage.bucket('yash-boldcare-testing-fine');
 @Resolver()
 export class AppResolver {
   @Query(() => String)
   sayHello(): string {
+    async function listBuckets() {
+      try {
+        const results = await storage.getBuckets();
+
+        const [buckets] = results;
+
+        buckets.forEach((bucket) => {
+          console.log(bucket.name);
+        });
+      } catch (err) {
+        console.error('ERROR:', err);
+      }
+    }
+    listBuckets();
+
     return 'Hello World!';
   }
 
-  @Mutation(() => Boolean)
+  @Mutation(() => String)
   async singleUpload(
     @Args({ name: 'file', type: () => GraphQLUpload })
     file: FileUpload,
-  ): Promise<boolean> {
-    return new Promise(async (resolve, reject) =>
+  ) {
+    await new Promise((resolves, rejects) =>
       file
         .createReadStream()
-        .pipe(createWriteStream(`./uploads/${file.filename}`))
-        .on('finish', () => resolve(true))
-        .on('error', () => reject(false)),
+        .pipe(
+          bucket2.file(`check/${file.filename}`).createWriteStream({
+            resumable: false,
+            gzip: true,
+          }),
+        )
+        .on('error', (err: any) => rejects(err))
+        .on('finish', resolves),
     );
+
+    // For bucket and bucket1 we can't use below line code
+    await bucket2.file(`check/${file.filename}`).makePublic();
+
+    return `https://storage.cloud.google.com/yash-boldcare-testing-fine/check/${file.filename}`;
   }
 
   @Mutation(() => Boolean)
@@ -30,24 +69,61 @@ export class AppResolver {
   ): Promise<boolean | string[]> {
     const newFiles = await Promise.all(files);
 
-    const x = await Promise.all(
+    await Promise.all(
       newFiles.map((file) => {
-        return new Promise<boolean>(async (resolve, reject) =>
+        return new Promise((resolves, rejects) =>
           file
             .createReadStream()
-            .pipe(createWriteStream(`./uploads/${file.filename}`))
-            .on('finish', () => resolve(true))
-            .on('error', () => reject(false)),
+            .pipe(
+              bucket.file(file.filename).createWriteStream({
+                resumable: false,
+                gzip: true,
+              }),
+            )
+            .on('error', (err: any) => rejects(err))
+            .on('finish', resolves),
         );
       }),
     );
 
-    console.log(newFiles);
-
-    console.log(x);
-
     return true;
   }
-}
 
-// { createReadStream, filename }: FileUpload,
+  @Mutation(() => String)
+  async getSignedUrl(
+    @Args({ name: 'file', type: () => GraphQLUpload })
+    file: FileUpload,
+  ) {
+    await new Promise((resolves, rejects) =>
+      file
+        .createReadStream()
+        .pipe(
+          bucket.file(file.filename).createWriteStream({
+            resumable: false,
+            gzip: true,
+          }),
+        )
+        .on('error', (err: any) => rejects(err))
+        .on('finish', resolves),
+    );
+
+    const [url] = await bucket
+      .file(encodeURIComponent(file.filename))
+      .getSignedUrl({
+        version: 'v4',
+        action: 'read',
+        expires: Date.now() + 1 * 60 * 1000,
+      });
+
+    return url;
+  }
+
+  @Query(() => String)
+  async getDownloadUrl(
+    @Args({ name: 'file', type: () => String }) file: string,
+  ) {
+    await bucket1.file(file).download({ destination: file });
+
+    return 'success';
+  }
+}
